defff652e78e9d53bf25b86757454c31edb1e8a7
Tom0017 <rspsmods@gmail.com>
2019-08-13 21:08:21 -0400

    Implement fastutil

--- src/main/kotlin/gg/rsmod/cache/osrs/InstructionMap.kt
+++ src/main/kotlin/gg/rsmod/cache/osrs/InstructionMap.kt
@@ -2,9 +2,9 @@ package gg.rsmod.cache.osrs
 
 import gg.rsmod.cache.io.ReadOnlyPacket
 import gg.rsmod.cache.osrs.config.ConfigType
-import gnu.trove.map.hash.TIntObjectHashMap
+import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
 
-class InstructionMap<T : ConfigType> : TIntObjectHashMap<(T).(ReadOnlyPacket) -> Unit>() {
+class InstructionMap<T : ConfigType> : Int2ObjectOpenHashMap<(T).(ReadOnlyPacket) -> Unit>() {
 
     fun register(instruction: Int, read: (T).(ReadOnlyPacket) -> Unit): RegisterMessage {
         val message = if (containsKey(instruction)) {
