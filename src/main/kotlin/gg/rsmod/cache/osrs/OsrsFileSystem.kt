cc77971100b241f6e1d77bb2e08666aa53cf139c
Tom0017 <rspsmods@gmail.com>
2019-12-06 20:37:11 -0500

    feat: Path param alias functions

--- src/main/kotlin/gg/rsmod/cache/osrs/OsrsFileSystem.kt
+++ src/main/kotlin/gg/rsmod/cache/osrs/OsrsFileSystem.kt
@@ -5,13 +5,17 @@ import com.github.michaelbull.result.andThen
 import gg.rsmod.cache.FileSystem
 import gg.rsmod.cache.domain.DomainMessage
 import gg.rsmod.cache.osrs.config.Js5Archive
+import java.nio.file.Path
 
-internal const val EMPTY_STRING = ""
 internal const val NULL_STRING = "null"
 internal val EMPTY_SHORT_ARRAY = ShortArray(0)
 internal val EMPTY_INT_ARRAY = IntArray(0)
 internal val EMPTY_INT_INT_MAP = mutableMapOf<Int, Int>()
 internal val EMPTY_INT_STRING_MAP = mutableMapOf<Int, String>()
+
+fun FileSystem.Companion.buildOsrs(path: Path) = buildOsrs(path.toAbsolutePath().toString())
+
+fun FileSystem.Companion.preloadOsrs(path: Path) = preloadOsrs(path.toAbsolutePath().toString())
 
 fun FileSystem.Companion.buildOsrs(directory: String): Result<FileSystem, DomainMessage> =
     FileSystem.of {
